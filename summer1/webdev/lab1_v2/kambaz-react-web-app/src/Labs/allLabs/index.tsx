69700548d1beec8413cf4284c87fd0288a555636
kkporto <kakamportoo@gmail.com>
2025-05-19 16:16:03 -0400

    commit for new attempt, 5.19 #1 at deploying website

---
+++ summer1/webdev/lab1_v2/kambaz-react-web-app/src/Labs/allLabs/index.tsx
@@ -0,0 +1,10 @@
+export default function AllLabs() {
+return (
+  <div>
+  <h4>Anchor tag</h4>
+  Please  
+  <a href="https://github.com/kkporto/webdev-lab1" id="wd-lipsum">click here</a>
+    to get dummy text<br/>
+  </div>
+  );
+}
